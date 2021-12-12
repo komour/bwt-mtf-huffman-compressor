93d89a3230ef8a57db278853e8f6427bcedbc17c
komour <komaroff404@gmail.com>
2021-12-13 01:41:45 +0300

    Remove cmake-build-debug from .gitignore

---
+++ cmake-build-debug/CMakeFiles/BWT_MTF_Huffman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BWT_MTF_Huffman"
+  "BWT_MTF_Huffman.pdb"
+  "CMakeFiles/BWT_MTF_Huffman.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BWT_MTF_Huffman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
